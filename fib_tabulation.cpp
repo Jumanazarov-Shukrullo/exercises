dd1ce557a6487e10c07c67970f71157dce7c9771
Shukrullo <79999273+Jumanazarov-Shukrullo@users.noreply.github.com>
2021-11-15 10:39:07 +0300

    Create fib_tabulation.cpp

---
+++ fib_tabulation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+size_t fib(size_t n) {
+    std::vector<size_t> vec(n);
+    if (n <= 1) {
+        return n;
+    }
+    vec[0] = 0;
+    vec[1] = 1;
+    for (int i = 2; i <= n; ++i) {
+        vec[i] = vec[i - 1] + vec[i - 2];
+    }
+    return vec[n];
+}
+
+int main() {
+    size_t n;
+    std::cin >> n;
+    std::cout << fib(n);
+}
