dacaa1e6902ad8538f21a6c1a25a7c28ef44ba1c
Jumanazarov-Shukrullo <shukrullo.jumanazarov@phystech.edu>
2021-12-10 13:27:52 +0300

    Added Algebra first hometask

---
+++ group.cpp
@@ -0,0 +1,112 @@
+/// Program that check given set of numbers
+/// and dictionary make group or not
+
+#include <bits/stdc++.h>
+
+typedef std::pair<size_t, size_t> Pair;
+
+bool IsGroup(std::set<size_t> &set, std::map<Pair, size_t> &map) {
+    size_t inverse_element = 0;
+    size_t neutral_element;
+    size_t counter = 0;
+    /// in case dictionary is empty we don't do anything
+    if (map.empty()) {
+        return false;
+    }
+    /// First check the size of the our dictionary with the elements of the set
+    if (map.size() != (size_t) std::pow(set.size(), 2))
+        return false;
+    std::vector<size_t> vec;
+    for (auto i: set) {
+        for (auto j: set) {
+            if (!set.contains(map[std::make_pair(i, j)])) {
+                return false;
+            }
+            for (auto k: set) {
+                size_t first_temp = map[std::make_pair(i, j)];
+                size_t second_temp = map[std::make_pair(j, k)];
+                if (map[std::make_pair(first_temp, k)] != map[std::make_pair(i, second_temp)]) {
+                    return false;
+                }
+            }
+            if (map[std::make_pair(i, j)] == i && counter == 0) {
+                neutral_element = j;
+                counter++;
+            }
+            if (map[std::make_pair(i, j)] == i && counter > 0 && j != neutral_element)
+                return false;
+            if (map[std::make_pair(i, j)] == neutral_element)
+                inverse_element++;
+        }
+    }
+    if (inverse_element != set.size())
+        return false;
+    return true;
+}
+
+bool IsAbelian(std::set<size_t> &vector, std::map<Pair, size_t> &map) {
+
+    /// firstly check whether it is a group or not
+    if (!IsGroup(vector, map)) {
+        return false;
+    }
+    /// Check whether it has additivity property
+    for (auto i: vector) {
+        for (auto j: vector) {
+            if (map[std::make_pair(i, j)] != map[std::make_pair(j, i)]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+void Solve() {
+    size_t size_of_map;
+    std::cout << "Enter size of map: ";
+    std::cin >> size_of_map;
+    std::set<size_t> set = {0, 1, 2, 3, 4};
+    std::map<Pair, size_t> dict;
+    /// Make a dictionary
+    for (int i = 0; i < size_of_map; ++i) {
+        size_t m, n, value;
+        std::cin >> m >> n >> value;
+        dict.insert(std::make_pair(std::make_pair(m, n), value));
+    }
+    std::cout << IsGroup(set, dict) << "\n";
+    std::cout << IsAbelian(set, dict);
+}
+
+int main() {
+    Solve();
+    return 0;
+}
+/// Sample input, you can easily copy and paste to check for the being group
+
+/**
+0 0 0
+0 1 1
+0 2 2
+0 3 3
+0 4 4
+1 0 1
+1 1 2
+1 2 3
+1 3 4
+1 4 0
+2 0 2
+2 1 3
+2 2 4
+2 3 0
+2 4 1
+3 0 3
+3 1 4
+3 2 0
+3 3 1
+3 4 2
+4 0 4
+4 1 0
+4 2 1
+4 3 2
+4 4 3
+ **/
