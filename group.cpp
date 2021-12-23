f79e450a8b051e19d099a7e94b296c020297d7fe
Shukrullo <79999273+Jumanazarov-Shukrullo@users.noreply.github.com>
2021-12-23 14:58:38 +0300

    Update group.cpp

--- group.cpp
+++ group.cpp
@@ -33,15 +33,25 @@ bool IsGroup(std::set<size_t> &set, std::map<Pair, size_t> &map) {
             }
             /// if i find the neutral element in the set i will make equal to the second key of dictionary
             /// the purpose of the increasing the counter one is that when i find the different  neutral element
-            /// i will return false after this if because of uniqueness of the neutral element 
+            /// i will return false after this if because of uniqueness of the neutral element
             if (map[std::make_pair(i, j)] == i && counter == 0) {
                 neutral_element = j;
-                counter++;
+                counter = 1;
             }
-            /// why counter is >0 because i set is as 1 and when i come here and check whether i have 2 different 
+
+            if (map[std::make_pair(i, j)] == j && counter == 0) {
+                neutral_element = i;
+                counter = 1;
+            }
+
+            /// why counter is >0 because i set is as 1 and when i come here and check whether i have 2 different
             /// neutral elements and i return false
             if (map[std::make_pair(i, j)] == i && counter > 0 && j != neutral_element)
                 return false;
+        }
+    }
+    for (auto i : set) {
+        for (auto j : set) {
             if (map[std::make_pair(i, j)] == neutral_element)
                 inverse_element++;
         }
@@ -73,7 +83,7 @@ void Solve() {
     size_t size_of_map;
     std::cout << "Enter size of map: ";
     std::cin >> size_of_map;
-    std::set<size_t> set = {0, 1, 2};
+    std::set<size_t> set = {1,4};
     std::map<Pair, size_t> dict;
     /// Make a dictionary
     for (int i = 0; i < size_of_map; ++i) {
@@ -97,32 +107,32 @@ int main() {
     Solve();
     return 0;
 }
-/// Sample input, you can easily copy and paste to check for the being group
-
-/**
-0 0 0
-0 1 1
-0 2 2
-0 3 3
-0 4 4
-1 0 1
-1 1 2
-1 2 3
-1 3 4
-1 4 0
-2 0 2
-2 1 3
-2 2 4
-2 3 0
-2 4 1
-3 0 3
-3 1 4
-3 2 0
-3 3 1
-3 4 2
-4 0 4
-4 1 0
-4 2 1
-4 3 2
-4 4 3
- **/
+/////// Sample input, you can easily copy and paste to check for the being group
+////
+///////**
+//////0 0 0
+//////0 1 1
+//////0 2 2
+//////0 3 3
+//////0 4 4
+//////1 0 1
+//////1 1 2
+//////1 2 3
+//////1 3 4
+//////1 4 0
+//////2 0 2
+//////2 1 3
+//////2 2 4
+//////2 3 0
+//////2 4 1
+//////3 0 3
+//////3 1 4
+//////3 2 0
+//////3 3 1
+//////3 4 2
+//////4 0 4
+//////4 1 0
+//////4 2 1
+//////4 3 2
+//////4 4 3
+////// **/
